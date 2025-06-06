b7bdb01f345f40e7655fa58332b75778f361bd44
jaeheonYoo325 <yoojh0325@gmail.com>
2025-06-06 19:47:59 +0900

    2025.06.06 스프링으로 전환하기.

--- src/main/java/com/springboot/springcore/OrderApp.java
+++ src/main/java/com/springboot/springcore/OrderApp.java
@@ -7,15 +7,22 @@ import com.springboot.springcore.member.MemberServiceImpl;
 import com.springboot.springcore.order.Order;
 import com.springboot.springcore.order.OrderService;
 import com.springboot.springcore.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
