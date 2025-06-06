558821d6ae52cf374f0e2fa8ffbde29315e28f9b
jaeheonYoo325 <yoojh0325@gmail.com>
2025-06-06 17:14:29 +0900

    2025.06.06 AppConfig 리팩터링.

--- src/main/java/com/springboot/springcore/AppConfig.java
+++ src/main/java/com/springboot/springcore/AppConfig.java
@@ -1,6 +1,8 @@
 package com.springboot.springcore;
 
+import com.springboot.springcore.discount.DiscountPolicy;
 import com.springboot.springcore.discount.FixDiscountPolicy;
+import com.springboot.springcore.member.MemberRepository;
 import com.springboot.springcore.member.MemberService;
 import com.springboot.springcore.member.MemberServiceImpl;
 import com.springboot.springcore.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import com.springboot.springcore.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
