fa57c923d49c3681e3a2bf37f38ad8ab61450a4b
jaeheonYoo325 <yoojh0325@gmail.com>
2025-06-06 09:13:01 +0900

    2025.06.06 새로운 할인 정책 개발.

---
+++ src/main/java/com/springboot/springcore/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package com.springboot.springcore.discount;
+
+import com.springboot.springcore.member.Grade;
+import com.springboot.springcore.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
