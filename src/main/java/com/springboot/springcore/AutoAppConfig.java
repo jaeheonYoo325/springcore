f8b51647d8bdef23cac645b6130c1513a267d1ed
jaeheonYoo325 <yoojh0325@gmail.com>
2025-06-07 20:32:36 +0900

    2025.06.07 중복 등록과 충돌.

--- src/main/java/com/springboot/springcore/AutoAppConfig.java
+++ src/main/java/com/springboot/springcore/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package com.springboot.springcore;
 
+import com.springboot.springcore.member.MemberRepository;
+import com.springboot.springcore.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -9,4 +12,12 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 수동 Bean 설정
+    // 수동 Bean 등록 vs 자동 Bean 등록, 충돌
+    // @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
 }
