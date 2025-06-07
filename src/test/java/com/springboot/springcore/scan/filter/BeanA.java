8d8b360fe6945e70e6c11754750336f9c2e319ef
jaeheonYoo325 <yoojh0325@gmail.com>
2025-06-07 20:21:45 +0900

    2025.06.07 필터.

---
+++ src/test/java/com/springboot/springcore/scan/filter/BeanA.java
@@ -0,0 +1,5 @@
+package com.springboot.springcore.scan.filter;
+
+@MyIncludeComponent
+public class BeanA {
+}
---
+++ src/test/java/com/springboot/springcore/scan/filter/BeanB.java
@@ -0,0 +1,5 @@
+package com.springboot.springcore.scan.filter;
+
+@MyExcludeComponent
+public class BeanB {
+}
