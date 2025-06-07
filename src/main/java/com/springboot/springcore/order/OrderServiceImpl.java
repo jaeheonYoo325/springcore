203119d4a3066743ea8056a6082312746902d9df
jaeheonYoo325 <yoojh0325@gmail.com>
2025-06-07 23:44:12 +0900

    2025.06.07 롬복과 최신 트랜드.

--- src/main/java/com/springboot/springcore/order/OrderServiceImpl.java
+++ src/main/java/com/springboot/springcore/order/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package com.springboot.springcore.order;
 import com.springboot.springcore.discount.DiscountPolicy;
 import com.springboot.springcore.member.Member;
 import com.springboot.springcore.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -14,11 +16,11 @@ public class OrderServiceImpl implements OrderService {
 
 
     // 생성자가 딱 1개 있으면, @Autowired 생략해도 자동 주입 된다
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
 // OCP 위반
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
