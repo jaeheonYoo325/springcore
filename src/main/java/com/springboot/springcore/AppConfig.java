f15958e805e49c95d0c2de9255132d762b0eff5e
jaeheonYoo325 <yoojh0325@gmail.com>
2025-06-07 12:01:26 +0900

    2025.06.07 @Configuration과 바이트코드 조작의 마법.

--- src/main/java/com/springboot/springcore/AppConfig.java
+++ src/main/java/com/springboot/springcore/AppConfig.java
@@ -18,7 +18,6 @@ public class AppConfig {
     // @Bean memberService -> new MemoryMemberRepository()
     // @Bean orderService -> new MemoryMemberRepository()
 
-
     // 예상 호출
     // call AppConfig.memberService
     // call AppConfig.memberRepository
@@ -30,7 +29,11 @@ public class AppConfig {
     // call AppConfig.memberService
     // call AppConfig.memberRepository
     // call AppConfig.orderService
-    
+    // 이미 스프링 컨테이너에 빈이 등록 되어 있으면, 스프링 컨테이너에서 찾아서 반환
+    // 스프링 컨테이너에 빈이 등록되어 있는게 없으면, 기존 로직 호출해서 빈을 생성하고 스프링 컨테이너에 등록
+
+    // @Configuration 지정안하면 예상호출처럼 호출되고 싱글톤 깨짐
+
     @Bean
     public MemberService memberService() {
         System.out.println("call AppConfig.memberService");
--- src/test/java/com/springboot/springcore/singleton/ConfigurationSingletonTest.java
+++ src/test/java/com/springboot/springcore/singleton/ConfigurationSingletonTest.java
@@ -23,11 +23,17 @@ public class ConfigurationSingletonTest {
 
         System.out.println("memberService -> memberRepository1 = " + memberRepository1);
         System.out.println("orderService -> memberRepository2 = " + memberRepository2);
-        System.out.println("=memberRepository = " + memberRepository);
+        System.out.println("memberRepository = " + memberRepository);
 
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
 
+    @Test
+    void configurationDeep() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
 
+        System.out.println("bean.getClass() = " + bean.getClass());
     }
 }
